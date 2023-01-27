669bb697804c0d187e166bbcfb496daa1c525ec0
al <sashasinitzin@gmail.com>
2023-01-28 00:32:07 +0300

    Working Dots ZoneCheker

--- scripts.js
+++ scripts.js
@@ -1,10 +1,16 @@
-const myRadioButton = document.getElementById("option1");
+
+
 const redZone = document.getElementById("redZone");
 const greenZone = document.getElementById("greenZone");
 const orangeZone = document.getElementById("orangeZone");
 const blueZone = document.getElementById("blueZone");
 const violetZone = document.getElementById("violetZone");
 
+const redCheckerColor = document.getElementById("redCheckerColor");
+const greenCheckerColor = document.getElementById("greenCheckerColor");
+const orangeCheckerColor = document.getElementById("orangeCheckerColor");
+const blueCheckerColor = document.getElementById("blueCheckerColor");
+const violetCheckerColor = document.getElementById("violetCheckerColor");
 
 document.querySelectorAll("input[name='content-options']").forEach(input => {
     input.addEventListener("change", (event) => {
@@ -17,6 +23,13 @@ document.querySelectorAll("input[name='content-options']").forEach(input => {
                     orangeZone.classList.remove("visible");
                     blueZone.classList.remove("visible");
                     violetZone.classList.remove("visible");
+
+                    redCheckerColor.classList.add("visible");
+
+                    greenCheckerColor.classList.remove("visible");
+                    orangeCheckerColor.classList.remove("visible");
+                    blueCheckerColor.classList.remove("visible");
+                    violetCheckerColor.classList.remove("visible");
                     break;
                 case "option2":
                     orangeZone.classList.add("visible");
@@ -25,6 +38,14 @@ document.querySelectorAll("input[name='content-options']").forEach(input => {
                     greenZone.classList.remove("visible");
                     blueZone.classList.remove("visible");
                     violetZone.classList.remove("visible");
+
+
+                    orangeCheckerColor.classList.add("visible");
+
+                    redCheckerColor.classList.remove("visible");
+                    greenCheckerColor.classList.remove("visible");
+                    blueCheckerColor.classList.remove("visible");
+                    violetCheckerColor.classList.remove("visible");
                     break;
                 case "option3":
                     greenZone.classList.add("visible");
@@ -33,6 +54,14 @@ document.querySelectorAll("input[name='content-options']").forEach(input => {
                     orangeZone.classList.remove("visible");
                     blueZone.classList.remove("visible");
                     violetZone.classList.remove("visible");
+
+
+                    greenCheckerColor.classList.add("visible");
+
+                    redCheckerColor.classList.remove("visible");
+                    orangeCheckerColor.classList.remove("visible");
+                    blueCheckerColor.classList.remove("visible");
+                    violetCheckerColor.classList.remove("visible");
                     break;
                 case "option4":
                     blueZone.classList.add("visible");
@@ -41,6 +70,14 @@ document.querySelectorAll("input[name='content-options']").forEach(input => {
                     greenZone.classList.remove("visible");
                     orangeZone.classList.remove("visible");
                     violetZone.classList.remove("visible");
+
+
+                    blueCheckerColor.classList.add("visible");
+
+                    redCheckerColor.classList.remove("visible");
+                    greenCheckerColor.classList.remove("visible");
+                    orangeCheckerColor.classList.remove("visible");
+                    violetCheckerColor.classList.remove("visible");
                     break;
                 case "option5":
                     violetZone.classList.add("visible");
@@ -49,9 +86,16 @@ document.querySelectorAll("input[name='content-options']").forEach(input => {
                     greenZone.classList.remove("visible");
                     orangeZone.classList.remove("visible");
                     blueZone.classList.remove("visible");
+
+
+                    violetCheckerColor.classList.add("visible");
+
+                    redCheckerColor.classList.remove("visible");
+                    greenCheckerColor.classList.remove("visible");
+                    orangeCheckerColor.classList.remove("visible");
+                    blueCheckerColor.classList.remove("visible");
                     break;
             }
-            // redZone.classList.remove("visible")
         }
     });
 });
